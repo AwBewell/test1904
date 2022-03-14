bb7e940094a75b6b755167c1ff24438cbb907b1a
zs <zs@bjpowernode.com>
2022-03-14 14:37:08 +0800

    1231212

--- src/com/bjpowernode/Student.java
+++ src/com/bjpowernode/Student.java
@@ -2,6 +2,7 @@ package com.bjpowernode;
 
 public class Student {
     private String name;
+    private String address;
     public static void main(String[] args) {
         System.out.println("zhangsan");
     }
